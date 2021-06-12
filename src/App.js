6ec5233d1a6ed10e799e632b7cdba1543b97590d
ItShivani <shivanisai26@gmail.com>
2021-06-12 15:48:44 +0530

    Stripe integrated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 const App = () => {
 	const [items,setItems] = useState([]);
 	const [cart,setCart] = useState({});
+	const [order,setOrder] = useState({});
+	const [errorMessage,setErrorMessage] = useState('');
 
 	const fetchItems = async()=>{
 		const {data} = await commerce.products.list();
@@ -41,6 +43,20 @@ const App = () => {
 		const {cart} = await commerce.cart.empty();
 		setCart(cart)
 	}
+	const refreshCart = async () => {
+		const newCart = await commerce.cart.refresh();
+		setCart(newCart);
+	}
+	const CaptureCheckout = async (checkTokenId,newOrder) => {
+		try{
+			const incomingOrder = await commerce.checkout.capture(checkTokenId,newOrder)
+			setOrder(incomingOrder)
+			refreshCart();
+		}
+		catch(error){
+			setErrorMessage(error.data.error.message)
+		}
+	}
 
 	useEffect(()=>{
 		fetchItems();
@@ -65,7 +81,11 @@ const App = () => {
 					/>
 				</Route>
 				<Route exact path="/checkout"> 
-					<Checkout/>
+					<Checkout cart={cart} 
+					order={order}
+					OnCaptureCheckout={CaptureCheckout}
+					error={errorMessage}
+					/>
 
 				</Route>
 			</Switch>
--- src/components/Checkout_All/AddressForm.js
+++ src/components/Checkout_All/AddressForm.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import {InputLabel,Select,MenuItem,Button,Grid,Typography} from '@material-ui/core'
 
 import {useForm,FormProvider} from 'react-hook-form'
 import CustomField from './CustomTextField'
-
-const AddressForm = () => {
+import {commerce} from '../../lib/commerce'
+import {Link} from 'react-router-dom'
+const AddressForm = ({checkoutToken , next }) => {
 	const methods = useForm();
+	const shippingCountry='India'
 
 	return (
 		<>
@@ -13,21 +15,41 @@ const AddressForm = () => {
 				Shipping Address
 			</Typography>
 			<FormProvider {...methods}>
-				<form onSubmit=''>
+				<form onSubmit={methods.handleSubmit((data) => next({...data,shippingCountry}))}>
 					<Grid container spacing={3}>
+					
 						<CustomField required name='firstName' label='First Name'/> 
 						<CustomField required name='lastName' label='Last Name'/>
 						<CustomField required name='email' label='Email'/>
-						<CustomField required name='Address' label='Address'/>
-						<CustomField required name='city' label='City'/>
+						{/*<CustomField required name='Address' label='Address'/>*/}
+						<CustomField required name='block' label="Block [A-K]"/>
+						<CustomField required name="flatnumber" label="Flat Number"/>
+						{/*<CustomField required name='city' label='City'/>*/}
+						<Select value="Hyderabad" name='city'>
+						<MenuItem value="Hyderabad">
+							Hyderabad
+						</MenuItem>
+						</Select>
 						<CustomField required name='zip' label='ZIP'/>
-				
+						<Grid item xs={12} sm={6}>
+							<InputLabel> Country </InputLabel>
+
+							<Select value="India" name='country'>
+							<MenuItem value="India">
+							India 
+							</MenuItem>
+							</Select>
+						</Grid>
 					</Grid>
+					<br/>
+          			<div style={{ display: 'flex',justifyContent: 'space-between' }}> 
+            			<Button component={Link} variant="outlined" to="/cart">Back to Cart</Button>
+            			<Button type="submit" variant="contained" color="primary">Next</Button>
+          			</div>
 				</form>
 			</FormProvider>
-
 		</>
-		)
-}
+		);
+};
 
 export default AddressForm;
